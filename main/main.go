0e29fee30cc80d38ccd18b1422eaea9a1cde62cf
Hendrik Purmann <h.purmann@googlemail.com>
2022-08-30 16:17:30 +0200

    Bonus: Handle yaml file from flag

--- main/main.go
+++ main/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	urlshort "gophercises-urlshort"
 	"net/http"
+	"os"
 )
 
 func main() {
+	yamlFilePath := flag.String("yaml", "", "YAML file with mapping from path to redirect URL")
+	flag.Parse()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -16,15 +20,26 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
+	var yamlBytes []byte
+	if *yamlFilePath != "" {
+		fmt.Println("Found flag yaml")
+		content, err := os.ReadFile(*yamlFilePath)
+		if err != nil {
+			fmt.Println("Error reading file", yamlFilePath)
+		}
+		yamlBytes = content
+	} else {
+		// Build the YAMLHandler using the mapHandler as the
+		// fallback
+		yamlBytes = []byte(`
   - path: /urlshort
     url: https://github.com/gophercises/urlshort
   - path: /urlshort-final
     url: https://github.com/gophercises/urlshort/tree/solution
-  `
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+  `)
+	}
+
+	yamlHandler, err := urlshort.YAMLHandler(yamlBytes, mapHandler)
 	if err != nil {
 		panic(err)
 	}
